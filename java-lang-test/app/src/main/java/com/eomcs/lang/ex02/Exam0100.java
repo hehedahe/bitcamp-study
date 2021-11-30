57442e33eeb2feab2b46f9cb4fdc0db0e8968a46
lunisolar <smyekgo@gmail.com>
2021-11-30 20:37:24 +0900

    211130

--- java-lang-test/app/src/main/java/com/eomcs/lang/ex02/Exam0100.java
+++ java-lang-test/app/src/main/java/com/eomcs/lang/ex02/Exam0100.java
@@ -1,17 +1,19 @@
 package com.eomcs.lang.ex02;
 
-//# 주석 - 일반 주석
-//
-//- 코드에 대한 이해를 돕기 위해 붙이는 설명이다.
-//- 컴파일할 때 무시된다. 즉 .class 파일에 존재하지 않는다. 
-//
+
+// # 주석 
+// - 일반 주석 //
+// - 코드에 대한 이해를 돕기 위해 붙이는 설
+// - 컴파일할 때 무시된다. 즉 .class 파일에 존재하지 않는다.
+
 
 /*
-## 여러 줄 주석(traditional comment)
-- 주석의 끝 표시를 만날 때까지 주석으로 간주된다.
-- 여러 줄의 설명을 붙일 때 유용한다.
-- C, C++ 프로그래밍 언어에서 사용하는 주석 문법과 같다.
-*/
+  ## 여러 줄 주석(traditional comment)
+  - 주석의 끝 표시를 만날 때까지 주석으로 간주된다.
+  - 여러 줄의 설명을 붙일 때 유용
+  - C, C++ 프로그래밍 언어에서 사용하는 주석 문법과 같다.
+↓여러줄 주석 끝 표시
+ */
 
 //## 한 줄 주석(end-of-line comment)
 //- 줄이 끝날 때 까지 주석으로 간주한다.
@@ -24,10 +26,11 @@ public class Exam0100 {
   }
 }
 
-//## 실습
-//1) 컴파일하기
-//- $ javac -d bin/main -encoding UTF-8 src/main/java/com/eomcs/basic/ex02/Exam0100.java
-//
-//2) 실행하기
-//- $ java -cp bin/main com.eomcs.basic.ex02.Exam0100
-//
+/*
+ * ## 실습 
+ * 1) 컴파일하기 
+ * - $ javac -d bin/main -encoding UTF-8 src/main/java/com/eomcs/lang/ex02/Exam0100.java
+ * 
+ * 2) 실행하기 
+ * - $ java -cp bin/main com.eomcs.basic.ex02.Exam0100
+ */
