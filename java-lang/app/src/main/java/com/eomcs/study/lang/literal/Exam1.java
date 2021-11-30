3768389250a7401109f4021519151478a084a1ea
lazysun <yingke@naver.com>
2021-11-30 11:12:40 +0900

    211130

--- java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam1.java
+++ java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam1.java
@@ -1,4 +1,4 @@
-package com.eomcs.study.literal;
+package com.eomcs.study.lang.literal;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
