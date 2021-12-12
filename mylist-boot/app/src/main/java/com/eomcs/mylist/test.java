4f4a21c64f8971db28680950b7aecb36c43ce268
hehedahe <smyekgo@gmail.com>
2021-12-13 00:46:41 +0900

    contact test

--- mylist-boot/app/src/main/java/com/eomcs/mylist/test.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/test.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
 
   @GetMapping("/test/list")
-  public Object list2() {
+  public Object list() {
     String[] contacts = {
         "홍길동11, hong2@test.com, 010-2222-3333, 비트",
         "홍길동22, hong2@test.com, 010-2222-3333, 캠프",
         "홍길동33, hong2@test.com, 010-2222-3333, 멀어요!"
     };
     return contacts;
+  };
+
+  @GetMapping("/test/add")
+  public Object add(String name, String email, String tel, String company) {
+    String contact = name + "," + email + "," + tel + "," + company;
+    return contact;
   };
 };
