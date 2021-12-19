24251c4166dfdf9fd907e5591a4a8945282ac46b
hehedahe <smyekgo@gmail.com>
2021-12-19 19:11:24 +0900

    211217

--- java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0421Compute.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0421Compute.java
@@ -3,7 +3,7 @@ package com.eomcs.lang.ex07;
 // # 배열의 생성은 어디서 하는 것이 좋은가?
 // - 상황에 따라 적합한 것을 선택하면 된다.
 // - 정답은 없다!
-public class Exam0421 {
+public class Exam0421Compute {
 
   public static void main(String[] args) throws Exception {
 
@@ -16,17 +16,6 @@ public class Exam0421 {
     for (int i = 0; i < moneys.length; i++) {
       System.out.printf("%d => %.1f\n", moneys[i], totals[i]);
     }
-
-    System.out.println("---------------------");
-
-    // 메서드 쪽에서 결과를 담을 배열을 만들어 리턴하는 경우
-    float[] result;
-    result = compute2(moneys, 0.0089f);
-
-    for (int i = 0; i < moneys.length; i++) {
-      System.out.printf("%d => %.1f\n", moneys[i], result[i]);
-    }
-
   }
 
   static void compute(int[] moneys, float[] totals, float interest) {
@@ -34,15 +23,6 @@ public class Exam0421 {
       totals[i] = moneys[i] + (moneys[i] * interest);
     }
   }
-
-  static float[] compute2(int[] moneys, float interest) {
-    float[] totals = new float[moneys.length];
-    for (int i = 0; i < moneys.length; i++) {
-      totals[i] = moneys[i] + (moneys[i] * interest);
-    }
-    return totals;
-  }
-
 }
 
 
