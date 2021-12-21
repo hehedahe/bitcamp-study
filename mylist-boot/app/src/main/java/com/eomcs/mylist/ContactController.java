cfe4e0db4d0646a485ac81953a6c78c739770063
lunisolar <smyekgo@gmail.com>
2021-12-21 12:44:37 +0900

    211221

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -8,44 +8,52 @@ import org.springframework.web.bind.annotation.RestController;
 // 이 표시(애노테이션)가 붙어 있어야만 스프링부트가 이 클래스를 인식한다.
 public class ContactController {
 
-  String[] contacts = new String[5];
+  Contact[] contacts = new Contact[5];
   int size = 0; // 배열에 집어넣은 연락처 갯수
 
   @RequestMapping("/contact/list")
   public Object list() {
-    String[] arr = new String[size]; // 배열에 저장된 값만 복사할 새 배열을 만든다.
+    Contact[] arr = new Contact[size]; // 배열에 저장된 값만 복사할 새 배열을 만든다.
     for (int i = 0; i < size; i++) {
       arr[i]= contacts[i]; // 전체 배열에서 값이 들어있는 항목만 복사한다.
     }
     return arr; // 복사한 항목들을 담고있는 새 배열을 리턴한다.
   }
 
+
   @RequestMapping("/contact/add")
-  public Object add(String name, String email, String tel, String company) {
+  public Object add(Contact contact) {
+    //System.out.println(contact.toString());
+    System.out.println(contact); // 인스턴스의 값을 보고싶으면 ?
+    System.out.println(contact.email);
+
     if (size == contacts.length) { // 배열이 꽉 찼다면,
       contacts = grow(); // 메서드 이름에서 해당 코드에 대한 설명을 짐작할 수 있다. => 배열을 늘려라!
     }
-    contacts[size++] = createCSV(name, email, tel, company);
+
+    contacts[size++] = contact; // 인스턴스 주소(레퍼런스)가 들어옴
+
     return size;
   }
+
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
     int index = indexOf(email);
     if (index == -1) {
-      return 0;
+      return"";
     } //else { // else 생략 가능?
-    return contacts[index];
+    return contacts[index]; // Contact 인스턴스 리턴 => JSON형식 ?
     //}
   };
 
   @RequestMapping("contact/update")
-  public Object update(String name, String email, String tel, String company) {
-    int index = indexOf(email);
+  public Object update(Contact contact) {
+    int index = indexOf(contact.email);
     if (index == -1) {
       return 0;
     }
-    contacts[index] = createCSV(name, email, tel, company); // 사용자가 새로 입력하여 보낸 데이터 그 자리에 새 값으로 덮어쓰기
+    contacts[index] = contact; // 사용자가 새로 입력하여 보낸 데이터 그 자리에 새 값으로 덮어쓰기
     return 1; // 1이면 update됐다!
   };
 
@@ -60,17 +68,12 @@ public class ContactController {
   };
 
   // 기능:
-  // - 입력 받은 파라미터 값을 가지고 CSV 형식으로 문자열을 만들어준다.
-  String createCSV(String name, String email, String tel, String company) {
-    return name + "," + email + "," + tel + "," + company;
-  }
-
-  // 기능:
   // - 이메일로 연락처 정보를 찾는다.
   // - 찾은 연락처의 배열 인덱스를 리턴한다.
   int indexOf(String email) {
     for (int i = 0; i < size ; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {  // 예) "u1@test.com"
+      Contact contact = contacts[i];
+      if (contact.email.equals(email)) {  // 예) "u1@test.com"
         return i;
       }
     }
@@ -79,8 +82,8 @@ public class ContactController {
 
   // 기능:
   // - 배열에서 저장한 항목을 삭제한다.
-  String remove(int index) {
-    String old = contacts[index];
+  Contact remove(int index) {
+    Contact old = contacts[index];
     // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
     // 뒤에 있는 칭구들을 앞으로 땡겨와야하니 index는 삭제하는 다음 인덱스 기준 => index+1
     for (int i = index + 1; i < size ; i++) {
@@ -93,11 +96,11 @@ public class ContactController {
   // 기능:
   // - 배열의 크기를 늘린다.
   // - 기존 배열의 값을 복사해온다.
-  String[] grow() {
+  Contact[] grow() {
     // 기존 배열보다 50% 큰 배열을 새로 만든다.
     //int newCapacity = newLength();
     //String[] arr = new String[newCapacity];
-    String[] arr = new String[newLength()];
+    Contact[] arr = new Contact[newLength()];
     copy(contacts, arr);
     return arr; // 메모리 위치정보인 배열 주소가 리턴
   }
@@ -110,7 +113,7 @@ public class ContactController {
 
   // 기능:
   // - 배열을 복사한다.
-  void copy(String[] source, String[] target) { // void는 return값이 없음
+  void copy(Contact[] source, Contact[] target) { // void는 return값이 없음 // Contact 배열 => Contact 레퍼런스 배열
     // 개발자가 잘못 사용할 것을 대비해서 다음 코드를 추가한다.
     // 즉, target 배열이 source 배열보다 작을 경우 target 배열 크기만큼만 복사한다. 
     int length = source.length;
