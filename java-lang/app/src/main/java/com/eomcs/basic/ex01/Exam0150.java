e6d006b9b5b746f76a874377659be8a9b6414acc
hehedahe <smyekgo@gmail.com>
2022-01-20 16:46:36 +0900

    220120 Hashset, hashCode(), HashMap

--- java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0150.java
+++ java-lang/app/src/main/java/com/eomcs/basic/ex01/Exam0150.java
@@ -4,41 +4,43 @@ package com.eomcs.basic.ex01;
 import java.util.HashSet;
 
 public class Exam0150 {
-  
+
   static class Student {
     String name;
     int age;
     boolean working;
-    
+
     public Student(String name, int age, boolean working) {
       this.name = name;
       this.age = age;
       this.working = working;
     }
   }
-  
+
   public static void main(String[] args) {
-    
+
+    // s1 ~ s4 데이터가 같더라도 모두 다른 인스턴스
     Student s1 = new Student("홍길동", 20, false);
     Student s2 = new Student("홍길동", 20, false);
     Student s3 = new Student("임꺽정", 21, true);
     Student s4 = new Student("유관순", 22, true);
-    
+
     System.out.println(s1 == s2);
-    
+
+    // 인스턴스가 다르면 hash code도 다르다.
     System.out.println(s1.hashCode());
     System.out.println(s2.hashCode());
     System.out.println(s3.hashCode());
     System.out.println(s4.hashCode());
     System.out.println("--------------------");
-    
+
     // 해시셋(집합)에 객체를 보관한다.
     HashSet<Student> set = new HashSet<Student>();
     set.add(s1);
-    set.add(s2);
+    set.add(s2); // 이미 s2의 해시값과 같은 객체(s1)가 들어 있기 때문에 중복을 막기 위해 s2는 저장되지 않는다.
     set.add(s3);
     set.add(s4);
-    
+
     // 해시셋에 보관된 객체를 꺼낸다.
     Object[] list = set.toArray();
     for (Object obj : list) {
@@ -46,7 +48,7 @@ public class Exam0150 {
       System.out.printf("%s, %d, %s\n", 
           student.name, student.age, student.working ? "재직중" : "실업중");
     }
-    
+
     // 집합?
     // => 중복 값을 저장할 수 없다.
     // 
@@ -57,7 +59,7 @@ public class Exam0150 {
     // => 또한 hash 코드로 값을 저장할 인덱스를 결정하기 때문에
     //    값을 꺼낼 때 저장한 순서대로 꺼낼 수 없다.
     // 
-    
+
     // 현재 예제의 문제점?
     // => s1과 s2는 같은 데이터를 갖고 있지만, 다른 해시 코드를 리턴하기 때문에 
     //    HashSet 입장에서는 s1과 s2를 다른 값으로 취급한다.
