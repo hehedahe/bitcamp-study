cb91cf000863408b3b78e0572f045d5edae9a3b2
hehedahe <smyekgo@gmail.com>
2022-01-28 11:12:26 +0900

    ClientApp, ServerApp

--- project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
@@ -13,29 +13,33 @@ public class ServerApp {
     // 1) 클라이언트 App의 연결을 준비한다.
     ServerSocket serverSocket = new ServerSocket(8888);
 
-    // 2) 클라이언트의 연결 요청을 승인한다.
-    //    - 리턴 값은 클라이언트와 연결된 정보
-    System.out.println("클라이언트의 연결을 기다림!");
-    Socket socket = serverSocket.accept(); // 클라이언트가 연결될 때까지 리턴하지 않는다. 기다린다.
-    System.out.println("클라이언트와 연결됨!");
+    while (true) {
 
-    // 클라이언트와 데이터를 주고 받을 입출력 도구를 준비한다.
-    Scanner in = new Scanner(socket.getInputStream());
-    PrintStream out = new PrintStream(socket.getOutputStream());
+      // 2) 클라이언트의 연결 요청을 승인한다.
+      //    - 리턴 값은 클라이언트와 연결된 정보
+      System.out.println("클라이언트의 연결을 기다림!");
+      Socket socket = serverSocket.accept(); // 클라이언트가 연결될 때까지 리턴하지 않는다. 기다린다.
+      System.out.println("클라이언트와 연결됨!");
 
-    // 클라이언트와 주고 받는 순서가 맞아야 한다.
-    String request = in.nextLine();
-    System.out.println("이름: " + request);
+      // 클라이언트와 데이터를 주고 받을 입출력 도구를 준비한다.
+      Scanner in = new Scanner(socket.getInputStream());
+      PrintStream out = new PrintStream(socket.getOutputStream());
 
-    out.println(request + "님, 반갑습니다!");
+      // 클라이언트와 주고 받는 순서가 맞아야 한다.
+      String request = in.nextLine();
+      System.out.println("이름: " + request);
 
-    // 클라이언트와의 연결을 끊음
-    socket.close();
-    System.out.println("클라이언트 연결 종료!");
+      out.println("입닫고 어떻게 아바라를 먹나요 사다리 못타는 준호씨");
+
+      // 클라이언트와의 연결을 끊음
+      socket.close();
+      System.out.println("클라이언트 연결 종료!");
+    }
 
     // 서버쪽 연결 도구 종료!
-    serverSocket.close();
-    System.out.println("서버 종료!");
+    // -> 무한루프때문에 영원히 실행되지 않는다. => 주석처리함
+    //    serverSocket.close();
+    //    System.out.println("서버 종료!");
   }
 
 }
