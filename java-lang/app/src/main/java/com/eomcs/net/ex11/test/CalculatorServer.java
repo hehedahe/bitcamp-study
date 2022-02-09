9ffd0465be170c43f917fac2659f633b95033b37
hehedahe <smyekgo@gmail.com>
2022-02-09 14:14:37 +0900

    220208 Calculator Server/Client test

--- java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
@@ -1,26 +1,110 @@
 package com.eomcs.net.ex11.test;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class CalculatorServer {
 
-  public static void main(String[] args) {
+  String logo;
 
-    try (ServerSocket ss = new ServerSocket(8888);){
-      System.out.println("계산기 서버에 오신 걸 환영합니다!");
-      try (Socket socket = ss.accept();
-          BufferedReader in = new BufferedReader(new InputStream(socket.getInputStream()))
-          ){
+  public CalculatorServer() {
+    StringBuilder logoStr = new StringBuilder();
 
-      } catch (Exception e) {
+    try (BufferedReader welcomeIn = new BufferedReader(new FileReader("welcome.txt"));) {
+      while (true) {
+        String str = welcomeIn.readLine();
+        if (str == null) {
+          break;
+        }
+        logoStr.append(str + "\n");
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (Exception e) {
+      if (logoStr.length() > 0) {
+        logoStr.setLength(0);
+      }
+      logoStr.append("환영합니다!\n");
     }
 
+    logo = logoStr.toString();
+  }
+
+  public void launch(int port) {
+    try (ServerSocket ss = new ServerSocket(port);) {
+      System.out.println("서버 실행 중...");
+
+      while (true) {
+        new RequestHandler(ss.accept()).start();
+      }
+
+    } catch (Exception e) {
+      System.out.println("서버 소켓 생성 중 오류 발생!");
+    }
+  }
+
+  public static void main(String[] args) {
+    new CalculatorServer().launch(8888);
+  }
+
+  class RequestHandler extends Thread {
+    Socket socket;
+
+    public RequestHandler(Socket socket) {
+      this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+      try (
+          Socket socket2 = socket;
+          Scanner in = new Scanner(socket2.getInputStream());
+          PrintStream out = new PrintStream(socket2.getOutputStream());
+          ) {
+
+        out.print(logo);
+
+        out.println("계산식을 입력하세요!");
+        out.println("예) 10 + 15 ");
+        out.println();
+
+        while (true) {
+          String str = in.nextLine();
+          if (str.equals("quit")) {
+            out.println("Goodbye!");
+            out.flush();
+            break;
+          }
+
+          try {
+            String[] values = str.split(" ");
+            int a = Integer.parseInt(values[0]);
+            int b = Integer.parseInt(values[2]);
+            String op = values[1];
+
+            switch (op) {
+              case "+": out.printf("%d %s %d = %d\n", a, op, b, a + b); break;
+              case "-": out.printf("%d %s %d = %d\n", a, op, b, a - b); break;
+              case "*": out.printf("%d %s %d = %d\n", a, op, b, a * b); break;
+              case "/": out.printf("%d %s %d = %d\n", a, op, b, a / b); break;
+              case "%": out.printf("%d %s %d = %d\n", a, op, b, a % b); break;
+              default:  out.printf("%d %s %d = %d\n", a, op, b, "지원하지 않는 연산입니다.");
+            }
+            out.flush();
+
+          } catch (Exception e) {
+            out.println("계산 중 오류 발생 -" + e.getMessage());
+            out.flush();
+          }
+        }
+
+      } catch (Exception e) {
+        System.out.println("클라이언트 요청 처리 중 오류 발생!");
+      }
+
+
+    }
   }
 }
