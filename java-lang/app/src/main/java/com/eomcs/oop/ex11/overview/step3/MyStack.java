00055af9ce564ed861bbb822b1e6345a188522d9
hehedahe <smyekgo@gmail.com>
2022-02-10 15:48:15 +0900

    220210 collection 객체의 데이터 조회 - 3단계

--- java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step3/MyStack.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex11/overview/step3/MyStack.java
@@ -14,4 +14,9 @@ public class MyStack extends MyList {
     }
     return remove(size - 1); // 마지막부터 꺼낸다.
   }
+
+  @Override
+  public Iterator iterator() {
+    return new StackIterator(this); // 상속 받은 메서드 중에서 서브 클래스의 역할에 맞게 메서드를 재정의
+  }
 }
