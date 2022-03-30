b09f6c1ac1f25ebff80891d70cd4bc354a23dd32
hehedahe <smyekgo@gmail.com>
2022-03-30 11:21:39 +0900

    220330 로그인 구현

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/MemberDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/MemberDao.java
@@ -1,12 +1,16 @@
 package com.eomcs.mylist.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import com.eomcs.mylist.domain.Member;
 
 @Mapper
 public interface MemberDao {
 
   int insert(Member member);
+
+  Member findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+  // => 파라미터를 두 개 이상 넘길 때, SQL Mapper 에서 "xx" 라는 이름으로 파라미터를 꺼내달라고 요청 
 
   //  List<Member> findAll();
   //
--- mylist-boot/app/src/main/java/com/eomcs/mylist/service/MemberService.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/service/MemberService.java
@@ -5,6 +5,9 @@ import com.eomcs.mylist.domain.Member;
 public interface MemberService {
 
   int add(Member member);
+
+  Member get(String email, String password);
+
 }
 
 
