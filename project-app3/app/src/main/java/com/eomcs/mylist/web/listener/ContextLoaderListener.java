43397a3ec17b2ec75aedd859112a45a39478a46a
hehedahe <smyekgo@gmail.com>
2022-04-20 11:39:36 +0900

    220420 12.페이지 컨트롤러에 request handler 여러개 두기

--- project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/web/listener/ContextLoaderListener.java
@@ -110,31 +110,28 @@ public class ContextLoaderListener implements ServletContextListener {
         // 생성자를 호출하여 객첵를 생성한다.
         Object obj = constructor.newInstance(args);
 
+        // 클래스 정보에서 @RequestMapping 애노테이션을 추출한다.
+        String rootPath = "";
+        RequestMapping requestMappingAnno = classInfo.getAnnotation(RequestMapping.class);
+        if (requestMappingAnno != null) {
+          rootPath = requestMappingAnno.value();
+        }
+
         // @RequestMapping이 붙은 메서드를 알아낸다.
         Method[] methods = classInfo.getDeclaredMethods();
         //        Method requestHandler = null;
         for (Method m : methods) {
           RequestMapping anno = m.getAnnotation(RequestMapping.class);
+          String controllerPath = rootPath + anno.value();
           if (anno != null) {
-            //            RequestMappingHandler requestMappingHandler = new RequestMappingHandler();
-            //            requestMappingHandler.setObj(obj);
-            //            requestMappingHandler.setMethod(m);
-            //            requestMappingHandler.setPathInfo(compAnno.value());
-
             // 생성된 request handler 객체를 ServletContext 보관소에 담는다.
-            // 이때 이름은 @Component 애노테이션의 value 값을 사용한다.
-            //            sc.setAttribute(compAnno.value(), requestMappingHandler);
-            //
-            sc.setAttribute(compAnno.value(), new RequestMappingHandler()
+            sc.setAttribute(controllerPath, 
+                new RequestMappingHandler()
                 .setObj(obj)
                 .setMethod(m)
-                .setPathInfo(compAnno.value()));
-            break;
+                .setPathInfo(controllerPath));
           }
         }
-
-
-
       }
     }
   }
